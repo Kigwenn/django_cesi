5dae49844b6d3aeb893bf7f34f0a090162f0f2b4
Vincent Lecourt <lecourt.vincent@free.fr>
2020-03-13 16:42:57 +0100

    form add update delete  Membre wip

--- cesi/membre/models.py
+++ cesi/membre/models.py
@@ -6,14 +6,14 @@ from django.db import models
 class Membre(models.Model):
     object = models.Manager()
 
-    nom = models.TextField()
-    prenom = models.TextField()
-    adresse = models.TextField()
-    cp = models.TextField()
-    tel = models.TextField()
-    date_naissance = models.DateTimeField()
-    date_entre = models.DateTimeField()
-    date_sortie = models.DateTimeField()
+    nom = models.TextField(null=True)
+    prenom = models.TextField(null=True)
+    adresse = models.TextField(null=True)
+    cp = models.TextField(null=True)
+    tel = models.TextField(null=True)
+    date_naissance = models.DateTimeField(null=True)
+    date_entre = models.DateTimeField(null=True)
+    date_sortie = models.DateTimeField(null=True)
 
     promotion = models.ForeignKey('promotion.Promotion', on_delete=models.CASCADE, null=True)
     # typeMembre = models.OneToOneField('typeMembre.TypeMembre', on_delete = models.CASCADE, related_name='+')
